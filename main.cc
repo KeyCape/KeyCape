ef0b778330c6e54c689bcd8117a683b28171af7c
jesper-sec <jesper-sec@ogkw.de>
2023-03-10 13:31:05 +0100

    Move config.json

--- main.cc
+++ main.cc
@@ -5,7 +5,7 @@ int main() {
   google::InitGoogleLogging("");
   FLAGS_logtostderr = 1;
   drogon::app().addListener("0.0.0.0", 80);
-  drogon::app().loadConfigFile("../config.json");
+  drogon::app().loadConfigFile("config.json");
   drogon::app().run();
   return 0;
 }
