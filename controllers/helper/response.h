991bca1824aefab582575e799f36ce995b2ad129
jesper-sec <jesper-sec@ogkw.de>
2023-03-10 13:31:04 +0100

    Add response helper

---
+++ controllers/helper/response.h
@@ -0,0 +1,6 @@
+drogon::HttpResponsePtr toError(drogon::HttpStatusCode st, std::string &&msg) {
+  auto response = drogon::HttpResponse::newHttpResponse();
+  response->setStatusCode(st);
+  response->setBody(msg);
+  return response;
+}
