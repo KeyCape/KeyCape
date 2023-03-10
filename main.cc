d08e3269ac197ed6e0bbf76fc67e0c2865a28510
jesper-sec <jesper-sec@ogkw.de>
2023-03-10 13:31:03 +0100

    Remove unused lines

--- main.cc
+++ main.cc
@@ -1,8 +1,6 @@
 #include <drogon/drogon.h>
-#include <webauthn.h>
 
 int main() {
-  auto wa = Webauthn{"Relying Party Name", "Relying Party Id"};
   drogon::app().addListener("0.0.0.0", 80);
   drogon::app().loadConfigFile("../config.json");
   drogon::app().run();
