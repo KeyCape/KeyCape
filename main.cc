46c4ee47e8660dff03a8cdd7aca9e9e12875dbff
jesper-sec <jesper-sec@ogkw.de>
2023-03-10 13:31:03 +0100

    Initialize WebAuthn

--- main.cc
+++ main.cc
@@ -2,6 +2,7 @@
 #include <webauthn.h>
 
 int main() {
+  auto wa = Webauthn{"Relying Party Name", "Relying Party Id"};
   drogon::app().addListener("0.0.0.0", 80);
   drogon::app().loadConfigFile("../config.json");
   drogon::app().run();
