e8d7531722ff09d7f19206a94d2fe17850578df7
jesper-sec <jesper-sec@ogkw.de>
2023-03-10 13:31:04 +0100

    Store the PubKeyCredOpt object in Redis

--- controllers/Register.cc
+++ controllers/Register.cc
@@ -18,16 +18,32 @@ void Register::begin(const HttpRequestPtr &req,
     resp = drogon::HttpResponse::newHttpResponse();
     resp->setBody("The username must NOT be empty");
     resp->setStatusCode(HttpStatusCode::k400BadRequest);
+    callback(resp);
+    return;
   }
 
   // PublicKeyCredentialCreationOptions as json
-  auto pubKeyCredOpt = this->webauthn.beginRegistration(name);
-  LOG_DEBUG << "Response " << pubKeyCredOpt->getJson()->toStyledString();
+  auto jsonPubKeyCredOpt = this->webauthn.beginRegistration(name)->getJson();
+  auto jsonResponse = jsonPubKeyCredOpt->toStyledString();
+  LOG_DEBUG << "Response " << jsonResponse;
 
   // Store the users registration data
-
-
-  callback(drogon::HttpResponse::newHttpJsonResponse(*pubKeyCredOpt->getJson()));
+  auto redisClient = app().getRedisClient();
+  redisClient->execCommandAsync(
+      [callback, js = *jsonPubKeyCredOpt](const drogon::nosql::RedisResult &r) {
+        callback(drogon::HttpResponse::newHttpJsonResponse(js));
+      },
+      [&callback](const std::exception &ex) {
+        LOG_ERROR
+            << "Redis client couldn't save the temporary registration data "
+            << ex.what();
+        auto resPtr = drogon::HttpResponse::newHttpResponse();
+        resPtr->setStatusCode(HttpStatusCode::k500InternalServerError);
+        resPtr->setBody("Internal database error");
+        callback(resPtr);
+      },
+      "set registration:%s %b", name.c_str(), jsonResponse.c_str(),
+      jsonResponse.size());
 }
 
 void Register::finish(const HttpRequestPtr &req,
