dbeda548362236c6c95c71ed23cdb02cae169bc4
jesper-sec <jesper-sec@ogkw.de>
2023-03-10 13:31:04 +0100

    Add glog

--- main.cc
+++ main.cc
@@ -1,6 +1,9 @@
 #include <drogon/drogon.h>
+#include <glog/logging.h>
 
 int main() {
+  google::InitGoogleLogging("");
+  FLAGS_logtostderr = 1;
   drogon::app().addListener("0.0.0.0", 80);
   drogon::app().loadConfigFile("../config.json");
   drogon::app().run();
